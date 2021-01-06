426969d30e73b3145aa1aef447ce6ee7fde7a299
Freddie Carthy <fcarthy@gmail.com>
2021-01-05 22:57:34 -0800

    simplify selector logic, update styles

--- index.js
+++ index.js
@@ -1,79 +1,72 @@
-let direction = 'left';
-const gridSize = 10;
-const snakeCoords = [{row: 7, column: 6}, {row: 7, column: 7}, {row: 7, column: 8}];
+let direction;
+let snakeCoords = [{row: 7, column: 6}, {row: 7, column: 7}, {row: 7, column: 8}]; // initial snake
+const gridSize = 50; // might  be nice to let the user set the grid size
 const generateGrid = (size) => {
   for (let row = 0; row < size; row++) {
-    for (let column = 0; column < gridSize; column++) {
+    for (let column = 0; column < size; column++) {
       const square = document.createElement('li');
-      square.setAttribute('id', `${row}${column}`);
       square.setAttribute('data-row', row);
       square.setAttribute('data-column', column);
-      square.classList.add('square');
-      // square.innerText = `${row}${column}`;
       document.getElementById('grid').append(square);
     }
   }
 }
-
 const moveSnake = () => {
-  [...document.getElementsByClassName("square")].forEach((square) => {
-    square.classList.remove("active")
+  [...document.getElementsByTagName('li')].forEach((square) => {
+    square.classList.remove('active')
   });
   snakeCoords.pop();
   switch(direction) {
     case 'up':
-      if (snakeCoords[0].row > 0) {
-        snakeCoords.splice(0, 0, {row: snakeCoords[0].row - 1, column: snakeCoords[0].column});
-      }
+      snakeCoords[0].row > 0 ? snakeCoords.splice(0, 0, { row: snakeCoords[0].row - 1, column: snakeCoords[0].column }) : lose();
       break;
     case 'right':
-      if (snakeCoords[0].column < gridSize) {
-        snakeCoords.splice(0, 0, {column: snakeCoords[0].column + 1, row: snakeCoords[0].row});
-      }
+      snakeCoords[0].column < gridSize - 1 ? snakeCoords.splice(0, 0, {column: snakeCoords[0].column + 1, row: snakeCoords[0].row}) : lose();
       break;
     case 'down':
-      if (snakeCoords[0].row < gridSize) {
-        snakeCoords.splice(0, 0, {row: snakeCoords[0].row + 1, column: snakeCoords[0].column});
-      }
+      snakeCoords[0].row < gridSize - 1 ? snakeCoords.splice(0, 0, {row: snakeCoords[0].row + 1, column: snakeCoords[0].column}) : lose();
       break;
     default: // left
-      if (snakeCoords[0].column > 0) {
-        snakeCoords.splice(0, 0, {column: snakeCoords[0].column - 1, row: snakeCoords[0].row});
-      }
-      break;
+      snakeCoords[0].column > 0 ? snakeCoords.splice(0, 0, {column: snakeCoords[0].column - 1, row: snakeCoords[0].row}) : lose();
   }
-
   for (let i = 0; i < snakeCoords.length; i++) {
-    const square = document.getElementById(`${snakeCoords[i].row}${snakeCoords[i].column}`);
-    square.classList.add("active");
+    const square = document.querySelector(`[data-row="${snakeCoords[i].row}"][data-column="${snakeCoords[i].column}"]`)
+    square.classList.add('active');
   }
 };
-
 document.addEventListener('keydown', event => {
-  switch (event.keyCode) {
-    case 75: // up
+  switch (event.key) {
+    case 'k': // up
       if (direction === 'down') break;
       direction = 'up';
       break;
-    case 76: // right
+    case 'l': // right
       if (direction === 'left') break;
       direction = 'right';
       break;
-    case 74: // down
+    case 'j': // down
       if (direction === 'up') break;
       direction = 'down';
       break;
-    case 72: // left
+    case 'h': // left
       if (direction === 'right') break;
       direction = 'left';
       break;
-    default:
-      break;
   }
 });
-
-setInterval(() => {
-  moveSnake();
-}, 250);
-
+let gameInterval;
+const lose = () => {
+  if (confirm('You\'ve lost. Would you like to start a new game?')) {
+    newGame();
+  } else {
+    clearInterval(gameInterval);
+  }
+}
+const newGame = () => {
+  direction = 'left';
+  snakeCoords = [{row: 7, column: 6}, {row: 7, column: 7}, {row: 7, column: 8}]; // initial snake
+  clearInterval(gameInterval);
+  gameInterval = setInterval(moveSnake, 150);
+}
 generateGrid(gridSize);
+newGame();
