c74087099b296b85ed043eba41e08a631e19fbe7
Freddie Carthy <fcarthy@gmail.com>
2021-01-09 13:37:48 -0800

    add apple logic

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let direction;
+let apple, appleInterval, direction, gameInterval, gridSize = 50;
 let snakeCoords = [{row: 7, column: 6}, {row: 7, column: 7}, {row: 7, column: 8}]; // initial snake
-const gridSize = 50; // might  be nice to let the user set the grid size
 const generateGrid = (size) => {
   for (let row = 0; row < size; row++) {
     for (let column = 0; column < size; column++) {
@@ -11,11 +10,23 @@ const generateGrid = (size) => {
     }
   }
 }
+document.addEventListener('keydown', event => {
+  if (event.key === 'i' && direction !== 'down') direction = 'up';
+  if (event.key === 'l' && direction !== 'left') direction = 'right';
+  if (event.key === 'k' && direction !== 'up') direction = 'down';
+  if (event.key === 'j' && direction !== 'right') direction = 'left';
+});
+const makeApple = () => ({ row: Math.floor(Math.random() * (gridSize - 1) + 1), column: Math.floor(Math.random() * (gridSize - 1) + 1)});
+const placeApple = () => {
+  if (apple) return;
+  let newApple = makeApple();
+  if (!snakeCoords.every(coord => coord.row !== newApple.row && coord.column !== newApple.column)) return placeApple();
+  apple = newApple; 
+  document.querySelector(`[data-row="${apple.row}"][data-column="${apple.column}"]`).classList.add('apple');
+};
+
 const moveSnake = () => {
-  [...document.getElementsByTagName('li')].forEach((square) => {
-    square.classList.remove('active')
-  });
-  snakeCoords.pop();
+  [...document.getElementsByTagName('li')].forEach((square) => square.classList.remove('active'));
   switch(direction) {
     case 'up':
       snakeCoords[0].row > 0 ? snakeCoords.splice(0, 0, { row: snakeCoords[0].row - 1, column: snakeCoords[0].column }) : lose();
@@ -29,44 +40,25 @@ const moveSnake = () => {
     default: // left
       snakeCoords[0].column > 0 ? snakeCoords.splice(0, 0, {column: snakeCoords[0].column - 1, row: snakeCoords[0].row}) : lose();
   }
+  if (!apple || (apple.row !== snakeCoords[0].row || apple.column !== snakeCoords[0].column)) {
+    snakeCoords.pop();
+  } else if (apple && (apple.row === snakeCoords[0].row && apple.column === snakeCoords[0].column)) {
+    snakeCoords.push({ row: snakeCoords[snakeCoords.length - 1].row + 1, column: snakeCoords[snakeCoords.length - 1].column });
+    document.querySelector(`[data-row="${apple.row}"][data-column="${apple.column}"]`).classList.remove('apple');
+    apple = null;
+  }
   for (let i = 0; i < snakeCoords.length; i++) {
-    const square = document.querySelector(`[data-row="${snakeCoords[i].row}"][data-column="${snakeCoords[i].column}"]`)
-    square.classList.add('active');
+    document.querySelector(`[data-row="${snakeCoords[i].row}"][data-column="${snakeCoords[i].column}"]`).classList.add('active');
   }
 };
-document.addEventListener('keydown', event => {
-  switch (event.key) {
-    case 'k': // up
-      if (direction === 'down') break;
-      direction = 'up';
-      break;
-    case 'l': // right
-      if (direction === 'left') break;
-      direction = 'right';
-      break;
-    case 'j': // down
-      if (direction === 'up') break;
-      direction = 'down';
-      break;
-    case 'h': // left
-      if (direction === 'right') break;
-      direction = 'left';
-      break;
-  }
-});
-let gameInterval;
-const lose = () => {
-  if (confirm('You\'ve lost. Would you like to start a new game?')) {
-    newGame();
-  } else {
-    clearInterval(gameInterval);
-  }
-}
+const lose = () => (confirm('You\'ve lost. Would you like to start a new game?')) ? newGame() : clearInterval(gameInterval);
 const newGame = () => {
   direction = 'left';
   snakeCoords = [{row: 7, column: 6}, {row: 7, column: 7}, {row: 7, column: 8}]; // initial snake
   clearInterval(gameInterval);
-  gameInterval = setInterval(moveSnake, 150);
+  clearInterval(appleInterval);
+  gameInterval = setInterval(moveSnake, 100);
+  appleInterval = setInterval(placeApple, 5000);
 }
 generateGrid(gridSize);
 newGame();
